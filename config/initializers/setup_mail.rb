b642adcdbd2d3a57f70410f458872190d5938280
Karthikeyan A K <mindaslab@gmail.com>
2013-03-26 10:36:21 +0530

    mail change

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,8 +1,7 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
+  :address              => "smtp.zoho.com",
+  :port                 => 465,
   :user_name            => APP_CONFIG["email"],
   :password             => APP_CONFIG["password"],
-  :authentication       => "plain",
   :enable_starttls_auto => true
 }
